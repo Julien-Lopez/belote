1cbb1839896770a00e38e3bb4fc45ad1168764a5
Julien Lopez <julien.lopez@lri.fr>
2016-03-14 12:39:21 +0100

    Implement Human.bet()

--- src/player/Human.scala
+++ src/player/Human.scala
@@ -1,12 +1,21 @@
 package player
 
-import game.{Color, Card, DIAMOND}
+import game._
 
 class Human(name_ : String) extends Player {
   override protected val name = name_
   override protected var cards : List[Card] = List.empty
 
-  override def bet(currentBet : (Int, Color)) =
-    if (currentBet == null) (80, DIAMOND) else if (currentBet._1 < 100) (currentBet._1 + 10, DIAMOND) else null
+  override def bet(currentBet : (Int, Color)) : (Int, Color) =
+  {
+    Console.println(name + " bets: ")
+    val (qty, color) = scala.io.StdIn.readf2("{0,number,integer} {1}")
+    (qty.asInstanceOf[Long].toInt, color.toString match {
+    case "DIAMOND" => DIAMOND
+    case "HEART" => HEART
+    case "SPADE" => SPADE
+    case "CLUB" => CLUB
+  })
+  }
   override def play() = cards.head
 }
